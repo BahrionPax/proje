b9d7d1e3fb954993af0d2bce54ee141537a3766f
BahrionPax <bahadirerce852@gmail.com>
2025-05-13 15:15:09 +0300

    ilk gönderim

---
+++ Models/OkulDb_1Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Okul.Models
+{
+    public class OkulDb_1Context : DbContext 
+    {
+        public DbSet<Ogrenci> Ogrenciler { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=SKILLPOINTSS\\SQLEXPRESSTABANI;Database=OkulDbMVC;Integrated Security=true;TrustServerCertificate=true");
+        }
+       
+    }
+}
